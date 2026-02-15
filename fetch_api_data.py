d6e302d92df8ba280b6a233446edf492783a959a
Amogelang Ngene <amglng9@gmail.com>
2026-02-15 14:56:08 +0200

    adding Docker containerization, Logging, Enviroment variables, Multi-ticker support.

--- fetch_api_data.py
+++ fetch_api_data.py
@@ -3,30 +3,57 @@ import requests
 
 API_URL = "https://pocketportfolio.app/api/tickers/FUN/json"
 
-
-#fetching the api data
 def extract():
-    response = response.get(API_URL)
-    return response.json()
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()  # checks for HTTP errors
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print("API request failed:", e)
+        return {}
 
 #transforming the data
 def transform(json_data): # Flatten JSON into a list of rows
-    rows = []
+    transformed = []
 
-    if "history_sample" in json_data:
-        for item in json_data["history_sample"]:
-            rows.append({
-            "symbol": json_data["symbol"],
-            "date": item.get("date", ""),
-            "open": item.get("open", ""),
-            "high": item.get("high", ""),
-            "low": item.get("low", ""),
-            "close": item.get("close", ""),
-            "volume": item.get("volume", "")
+    stock_data = json_data.get("data", [])
+    symbol = json_data.get("symbol", "UNKNOWN")
+
+    # if "history_sample" in json_data:
+    for row in stock_data:
+        transformed.append({
+            "symbol": symbol,
+            "date": row["date"],
+            "open": row["open"],
+            "high": row["high"],
+            "low": row["low"],
+            "close": row["close"],
+            "volume": row["volume"]
         })
-    print(rows)
-    return rows
+
+    return transformed
+    # print(rows)
+    # return rows
 
     # print(rows)
+def load(rows):
+    with open("api_data.csv", "w", newline= "") as f:
+        fieldnames = ["symbol", "date", "open", "high", "low", "close", "volume"]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(rows)
+
+    print("Data saved to api_data.csv, load stage complete...")
+
+def main():
+    json_data = extract()
+    rows = transform(json_data)
+    load(rows)
+
+if __name__ == "__main__":
+    main()
+
+
 
 
