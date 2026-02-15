fd17c39852e7f71af08aec7d07a9e1638ce30f64
Amogelang Ngene <amglng9@gmail.com>
2026-02-15 08:55:48 +0200

    data transformation

--- fetch_api_data.py
+++ fetch_api_data.py
@@ -9,4 +9,24 @@ def extract():
     response = response.get(API_URL)
     return response.json()
 
+#transforming the data
+def transform(json_data): # Flatten JSON into a list of rows
+    rows = []
+
+    if "history_sample" in json_data:
+        for item in json_data["history_sample"]:
+            rows.append({
+            "symbol": json_data["symbol"],
+            "date": item.get("date", ""),
+            "open": item.get("open", ""),
+            "high": item.get("high", ""),
+            "low": item.get("low", ""),
+            "close": item.get("close", ""),
+            "volume": item.get("volume", "")
+        })
+    print(rows)
+    return rows
+
+    # print(rows)
+
 
